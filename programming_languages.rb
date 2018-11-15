0dcbf300b9d8762e5dd315f9e98b29efbaf86d5b
Taimur Shah <tshah@scu.edu>
2018-11-15 22:09:30 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,20 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |stylez, name|
+    name.each do |key, value|
+      value.each do |one, two|
+        if key == :javascript
+          new_hash[key] = {
+            one => two,
+            :style => [:oo, :functional]
+          }
+        else
+          new_hash[key] = {
+            one => two,
+            :style => [stylez]}
+        end
+      end
+    end
+  end
+  new_hash
 end
